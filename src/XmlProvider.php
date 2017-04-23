60394a204a9ffd1cd8e4902c89bc162b0c39e27b
houdunwang <2300071698@qq.com>
2017-04-23 20:00:04 +0800

    no message

--- src/XmlProvider.php
+++ src/XmlProvider.php
@@ -7,21 +7,27 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\xml;
 
 use houdunwang\framework\build\Provider;
 
-class XmlProvider extends Provider {
+class XmlProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	//延迟加载
-	public $defer = true;
+    public function boot()
+    {
+    }
 
-	public function boot() {
-	}
-
-	public function register() {
-		$this->app->single( 'Xml', function () {
-			return new Xml();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'Xml',
+            function () {
+                return new Xml();
+            }
+        );
+    }
 }
